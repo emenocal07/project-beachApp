e0baf46f27f5822a9aa9cc35035719f4c9e3b07d
Ernesto Espinoza M <emenocal07@gmail.com>
2022-02-14 12:45:39 +0100

    Session

--- models/Beach.model.js
+++ models/Beach.model.js
@@ -1,6 +1,10 @@
 const { Schema, model } = require("mongoose");
 const beachSchema = new Schema(
     {
+        beachImg: {
+            type: String,
+            default: "https://i.pinimg.com/564x/db/ff/00/dbff0088ee5a4879cc3211e237b8f941.jpg",
+        },
     },
 )
 
