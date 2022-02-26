d1f6cfef1288d0c61a1f1bbeefc6ed084ceea835
Ernesto Espinoza M <emenocal07@gmail.com>
2022-02-27 00:06:27 +0100

    Correcciones md y mongo

--- config/session.config.js
+++ config/session.config.js
@@ -14,12 +14,11 @@ module.exports = (app) => {
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        maxAge: 600000000,
+        maxAge: 600000,
       },
       store: MongoStore.create({
         mongoUrl:
-          process.env.MONGODB_URI ||
-          "mongodb+srv://emenocal07:dg5WjQkxvN4uJHiH@cluster0.wmhyh.mongodb.net/beachapp",
+          process.env.MONGODB_URI
       }),
     })
   );
--- public/js/beaches-map.js
+++ public/js/beaches-map.js
@@ -34,3 +34,4 @@ function printMarkers() {
         title: beachTitle
     })
 }
+
